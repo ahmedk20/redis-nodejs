97d89bb11b626e1f46c012ea455161d8526f0f66
ahmed khaled <ahmedkhaled@ahmeds-MacBook-Air.local>
2025-08-18 22:36:16 +0300

    [any message]

--- app/main.js
+++ app/main.js
@@ -6,7 +6,7 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   connection.on("PING", () => {
-    connection.write("+PONG\r\n");
+    connection.write(`+PONG\r\n`);
   });
 });
 
