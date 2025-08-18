eb9bb671cc3bf4045d7eb1a03bd9c5339e96ede0
ahmed khaled <ahmedkhaled@ahmeds-MacBook-Air.local>
2025-08-18 23:07:29 +0300

    [any message]

--- app/main.js
+++ app/main.js
@@ -4,13 +4,17 @@ console.log("Logs from your program will appear here!");
 
 const server = net.createServer((connection) => {
   connection.on("data", (data) => {
-    const message = data.toString().trim();
+    const message = data.toString();
 
-    if (message === "PING") {
+    // Break the RESP message into parts
+    const parts = message.split("\r\n");
+    // Example for ECHO pear: ["*2", "$4", "ECHO", "$4", "pear", ""]
+
+    if (parts[2] && parts[2].toUpperCase() === "PING") {
       connection.write("+PONG\r\n");
-    }
-    if (message === "ECHO") {
-      connection.write(`$3\r\nhey\r\n`);
+    } else if (parts[2] && parts[2].toUpperCase() === "ECHO") {
+      const echoMessage = parts[4]; // this is the string after ECHO
+      connection.write(`$${echoMessage.length}\r\n${echoMessage}\r\n`);
     }
   });
 });
