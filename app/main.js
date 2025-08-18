c52b57978d810824925ecd4876f24330eb9255d9
ahmed khaled <ahmedkhaled@ahmeds-MacBook-Air.local>
2025-08-18 22:34:22 +0300

    [any message]

--- app/main.js
+++ app/main.js
@@ -7,7 +7,7 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((connection) => {
   connection.on("PING", () => {
     connection.write("+PONG\r\n");
-
+  });
 });
 
 server.listen(6379, "127.0.0.1");
